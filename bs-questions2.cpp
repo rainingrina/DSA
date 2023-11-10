8343b66eff99bb2fc2a2d8fb3650559db44d18f8
Raina Dsouza <140249054+rainingrina@users.noreply.github.com>
2023-11-10 10:33:23 +0530

    sqroot using bs

---
+++ bs-questions2.cpp
@@ -0,0 +1,19 @@
+//https://www.codingninjas.com/studio/problems/square-root_893351?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2&leftPanelTabValue=SUBMISSION
+int floorSqrt(int n)
+{
+     int low = 1, high = n;
+    //Binary search on the answers:
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
+        if (val <= (long long)(n)) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
+    }
+    return high;
+}
